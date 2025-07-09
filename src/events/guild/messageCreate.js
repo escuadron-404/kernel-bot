b8cd24581a027683fd9407fb4cf04ac68e6ee8bd
Ruben Vasquez <ruvasqm@gmail.com>
2025-07-09 17:19:23 -0400

    chore: formatting 418 event

--- src/events/guild/messageCreate.js
+++ src/events/guild/messageCreate.js
@@ -1,4 +1,4 @@
-const { Events, MessageFlags } = require("discord.js");
+const { Events } = require("discord.js");
 const COFFEE_KEYWORDS =
 	/\b(?:cafecito|cafetera|cafes|café|iced coffee|flat white|cold brew|drip coffee|turkish coffee|french press|clever dripper|coffee maker|coffee grinder|coffee beans|coffee grounds|coffee break|coffee shop|coffee pot|pour over|aeropress|cappuccino|percolator|americano|macchiato|cafetière|espresso|cortado|caffeine|affogato|ristretto|frappe|siphon|chemex|barista|arabica|robusta|greca|geisha|decaf|latte|mocha|lungo|kaffe|moka|café|cafe|coffee|beans|bean|brew|roast)\b/i;
 
@@ -6,7 +6,9 @@ module.exports = {
 	name: Events.MessageCreate,
 	async execute(message) {
 		// 1. Ignore messages from the bot itself to prevent infinite loops
-		if (message.author.bot) return;
+		if (message.author.bot) {
+			return;
+		}
 
 		// 2. Check if the message content matches any of the coffee keywords
 		if (COFFEE_KEYWORDS.test(message.content)) {
