080f3315bbc5a72ba9594cc3d839ce4586dec3e9
Ruben Vasquez <ruvasqm@gmail.com>
2025-07-09 17:45:18 -0400

    feat: logging for 418

--- src/events/guild/messageCreate.js
+++ src/events/guild/messageCreate.js
@@ -5,18 +5,14 @@ const COFFEE_KEYWORDS =
 module.exports = {
 	name: Events.MessageCreate,
 	async execute(message) {
-		// 1. Ignore messages from the bot itself to prevent infinite loops
 		if (message.author.bot) {
 			return;
 		}
-
-		// 2. Check if the message content matches any of the coffee keywords
 		if (COFFEE_KEYWORDS.test(message.content)) {
-			// If a keyword is found, send the "teapot" message in the same channel
 			message.channel.send("HTTP/1.1 418 I'm a teapot");
-			// console.log(
-			// 	`Detected coffee talk from ${message.author.tag} in #${message.channel.name}. Sent '418 I'm a teapot'.`,
-			// );
+			console.log(
+				`Detected coffee talk from ${message.author.tag} in #${message.channel.name}. Sent '418 I'm a teapot'.`,
+			);
 		}
 	},
 };
