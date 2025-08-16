27b9cf4e4f86171705c93dab638e5de96c06b56b
Sebastián Figueroa <sebaj348@gmail.com>
2025-08-15 20:57:12 -0400

    final Please enter the commit message for your changes. Lines starting

--- src/events/guild/guildNewMember.js
+++ src/events/guild/guildNewMember.js
@@ -2,11 +2,11 @@ module.exports = {
 	name: "guildMemberAdd",
 	once: false, // runs every time a user joins
 	execute(member) {
-		const channel = member.guild.systemChannel;
-		const welcomeChannel = "1389288057328500846";
-		if (channel) {
-			channel.send(
-				`Bienvenid@ ${member} al Escuadrón 404! No esperes y preséntate en <#${welcomeChannel}>🎉`,
+		const welcomeChannel = member.guild.systemChannel;
+		const presentationChannel = "1389288418764390611";
+		if (welcomeChannel) {
+			welcomeChannel.send(
+				`Bienvenid@ ${member} al Escuadrón 404! No esperes y preséntate en <#${presentationChannel}> 🎉`,
 			);
 		}
 	},
